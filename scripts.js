8735bf435531b7e394e554c0d22b8a6af074a52f
Gabriel <vdvsa159946@gmail.com>
2022-07-24 14:03:20 -0300

    feat: finish the funcionalities

--- scripts.js
+++ scripts.js
@@ -3,7 +3,9 @@ document.documentElement.className = ' js';
 import machineWritter from  './js/machine-writter.js';
 import initAnimateScroll from './js/animate-scroll.js';
 import initMenuMobile from './js/menu-mobile.js';
+import navigationTech from './js/tech-click.js';
 
 machineWritter('.nome-home');
 initAnimateScroll();
+navigationTech();
 initMenuMobile();
