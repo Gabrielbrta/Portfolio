7216fdaa46cfdb0c6dfebec46a2202bd2aafd4c8
Admin ASINF admin <admin@COHABSANTISTA.local>
2022-07-25 12:11:48 -0300

    refactor: change the scroll script

--- js/animate-scroll.js
+++ js/animate-scroll.js
@@ -2,16 +2,16 @@ import debounce from './dobounce.js';
 
 export default function initAnimateScroll() {
   const animateElements = document.querySelectorAll('[data-anime]');
-
-  if (animateElements.length) {
-    function handleAnime() {
-      animateElements.forEach((item) => {
-        const distanceItem = item.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight * 0.8;
-        if (distanceItem < windowHeight) item.classList.add('active');
-      });
-    }
-    handleAnime();
-    window.addEventListener('scroll', debounce(handleAnime, 200));
-  }
-}
+  window.onload = () => {
+    if (animateElements.length) {
+      function handleAnime() {
+        animateElements.forEach((item) => {
+          const distanceItem = item.getBoundingClientRect().top;
+          const windowHeight = window.innerHeight * 0.8;
+          if (distanceItem < windowHeight) item.classList.add('active');
+        });
+      }
+      window.addEventListener('scroll', debounce(handleAnime, 150));
+    };
+  };
+};
