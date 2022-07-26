0e4a47000ad09bbe523a0aa1016a28f82f9c1708
Admin ASINF admin <admin@COHABSANTISTA.local>
2022-07-26 09:38:43 -0300

    fix: refactory anime scroll

--- js/animate-scroll.js
+++ js/animate-scroll.js
@@ -2,16 +2,15 @@ import debounce from './dobounce.js';
 
 export default function initAnimateScroll() {
   const animateElements = document.querySelectorAll('[data-anime]');
-  window.onload = () => {
     if (animateElements.length) {
       function handleAnime() {
         animateElements.forEach((item) => {
           const distanceItem = item.getBoundingClientRect().top;
           const windowHeight = window.innerHeight * 0.8;
           if (distanceItem < windowHeight) item.classList.add('active');
+          else item.classList.remove('active')
         });
-      }
-      window.addEventListener('scroll', debounce(handleAnime, 150));
-    };
+      };
+    window.addEventListener('scroll', debounce(handleAnime, 150));
   };
 };
--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 import machineWritter from './js/machine-writter.js';
-import initAnimateScroll from './js/animate-scroll.js';
 import initMenuMobile from './js/menu-mobile.js';
 import navigationTech from './js/tech-click.js';
+import initAnimateScroll from './js/animate-scroll.js';
 
 document.documentElement.className = ' js';
 machineWritter('.nome-home');
-initAnimateScroll();
 navigationTech();
 initMenuMobile();
+initAnimateScroll();
